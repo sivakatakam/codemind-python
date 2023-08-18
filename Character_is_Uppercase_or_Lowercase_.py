e0a806243cbeba6a22d0f3a089aae949d21b50dd
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-08-18 15:05:38 +0530

    c=input() if c in 'ABCDEFGHIJKLMONPQRSTUVWXYZ' :     print("uppercase alphabet") elif c in 'abcdefghijklmnopqrstuvwxyz' :     print("lowercase alphabet") else :     print("not an alphabet")

---
+++ Character_is_Uppercase_or_Lowercase_.py
@@ -0,0 +1,7 @@
+c=input()
+if c in 'ABCDEFGHIJKLMONPQRSTUVWXYZ' :
+    print("uppercase alphabet")
+elif c in 'abcdefghijklmnopqrstuvwxyz' :
+    print("lowercase alphabet")
+else :
+    print("not an alphabet")
