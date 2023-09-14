5ae43b5a7e117ee2135239b8739cb61bc65c0eb8
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-09-14 23:41:51 +0530

    s=input() c=0 for i in range(len(s)):     o=ord(s[i])     if o>=48 and o<=57:         c=c+1 if c==0:     print("Doesn't contain digit") else:     print(f"Contains {c} digit")

---
+++ String_contain_digit_or_not.py
@@ -0,0 +1,10 @@
+s=input()
+c=0
+for i in range(len(s)):
+    o=ord(s[i])
+    if o>=48 and o<=57:
+        c=c+1
+if c==0:
+    print("Doesn't contain digit")
+else:
+    print(f"Contains {c} digit")
