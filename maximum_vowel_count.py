aa5f37498fbb18e69b941b802df01502982a5eb0
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-10-27 12:07:54 +0530

    def count(s):     v=['a', 'e', 'i', 'o', 'u']     c=0     for i in s:         if i in v:             c+=1     return c l=list(map(str,input().split())) x=[] for i in l:     x.append(count(i)) print(max(x))

---
+++ maximum_vowel_count.py
@@ -0,0 +1,12 @@
+def count(s):
+    v=['a', 'e', 'i', 'o', 'u']
+    c=0
+    for i in s:
+        if i in v:
+            c+=1
+    return c
+l=list(map(str,input().split()))
+x=[]
+for i in l:
+    x.append(count(i))
+print(max(x))
