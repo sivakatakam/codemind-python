2f4ff2ea005433a885d9f3ed34110e9b2963582f
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-11-03 15:08:56 +0530

    r,c=map(int,input().split()) m=[list(map(int,input().split())) for i in range(r)] e=0 o=0 for i in range(r):     for j in range(c):         if i%2==0:             e+=m[i][j]         else:             o+=m[i][j] print(e,o)

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
@@ -0,0 +1,11 @@
+r,c=map(int,input().split())
+m=[list(map(int,input().split())) for i in range(r)]
+e=0
+o=0
+for i in range(r):
+    for j in range(c):
+        if i%2==0:
+            e+=m[i][j]
+        else:
+            o+=m[i][j]
+print(e,o)
