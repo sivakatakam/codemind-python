3e43c89af5c27001536e164cda49480aaa14e509
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-11-09 15:25:30 +0530

    n=int(input()) s=0 m=1 while n>0:     r=n%10     n=n//10     s=s+r     m=m*r if s==m:     print("Spy Number") else:     print("Not Spy Number")

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,12 +1,12 @@
 n=int(input())
 s=0
-p=1
-while n!=0:
+m=1
+while n>0:
     r=n%10
     n=n//10
     s=s+r
-    p=p*r
-if s==p:
+    m=m*r
+if s==m:
     print("Spy Number")
 else:
     print("Not Spy Number")
